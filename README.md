aae0d3a023b0ed7787895b56e102a60d71dcfc2f
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-04-17 15:05:26 -0500

    create functions

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ install modules
 `$ npm install`
 
 place extra files we dont want to keep track in .gitignore
+
+# Project #
+
+This project will do the following
+- a city to be moving oposit directions like godzilla is walking
+- somethin poping up at a certain time from the back ground
+- it runs in a loop
+- people move on mouse over
+- dark grey clouds moving in the background
+- clouds moving same direction as godzilla
+- road, gozilla ontop
+
+## MUST HAVES
+    use of a conditional statement, and
+    use of a loop, and
+    use of a function that you write and call in the code.
+
+What you draw is up to you, but feel free to use these ideas as inspiration:
+
+    A city skyline that features some sort of movement –  clouds in the sky, cars on the street, etc.
+    An animation featuring a rising sun and a following sunset.
+    Abstract art that uses random numbers and loops to determine the placement and size of shapes and objects.
--- sketch.js
+++ sketch.js
@@ -10,10 +10,11 @@ function setup() {
 function draw() {
   //call your functions and do your drawing here.
   // please erase code below once you are working.
-  background(10);
-  fill(255, 255, 255, 255);
-  ellipse(30, 30, 30, 30);
-  twoCircles();
+  // background(10);
+  // fill(255, 255, 255, 255);
+  // ellipse(30, 30, 30, 30);
+  // twoCircles();
+  runGodZillaAttacks();
 }
 
 // write functions here that are called in your setup or draw function. please remove this one.
@@ -23,3 +24,55 @@ const twoCircles = () => {
   fill(0, 255, 0);
   ellipse(20, 20, 20, 20);
 };
+
+// use of a conditional statement, and
+// use of a loop, and
+// use of a function that you write and call in the code.
+
+// What you draw is up to you, but feel free to use these ideas as inspiration:
+
+// A city skyline that features some sort of movement –  
+// - clouds in the sky, cars on the street, etc.
+// An animation featuring 
+// - a rising sun 
+// - and a following sunset.
+// Abstract art
+//  - that uses random numbers and loops to determine the placement and size of shapes and objects.
+runGodZillaAttacks = () => {
+  setBackground();
+}
+
+// This project will do the following
+// - a city to be moving oposit directions like godzilla is walking
+// set background
+function setBackground() {
+  // https://processing.org/reference/background_.html
+  // relative link
+  img = loadImage("images/background.jpg");
+  background(img);
+}
+// put the cloudds
+function drawClouds() {
+}
+// draw skyline
+function drawSkyline(){
+
+}
+// draw road
+function drawRoad(){
+
+}
+// draw Godzilla
+function drawGozilla(){
+
+}
+// draw people
+function drawPeople(){
+
+}
+  // - somethin poping up at a certain time from the back ground
+  // - it runs in a loop
+  // - people move on mouse over
+  // - dark grey clouds moving in the background
+  // - clouds moving same direction as godzilla
+  // - road, gozilla ontop
